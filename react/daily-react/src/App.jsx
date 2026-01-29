93b3f2cad773f8a3c4709a5a966c75db9391c19e
abraaogg <abraaogoncalves1022@gmail.com>
2026-01-29 13:28:34 -0300

    update

--- react/daily-react/src/App.jsx
+++ react/daily-react/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Inputs from './Inputs & Controlled State(2)/Inputs'
-import List from './List(3)/List'
+import Inputs from './day2/Inputs & Controlled State/Inputs'
+import List from './day3/List/List'
 
 function App() {
   const [count, setCount] = useState(0)
