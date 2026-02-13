f594a040f84db3beb9814447982bd1895c993775
abraaogg <abraaogoncalves1022@gmail.com>
2026-02-13 10:52:26 -0300

    Update MiniFetch.jsx

--- react/daily-react/src/day11/MiniFetch.jsx
+++ react/daily-react/src/day11/MiniFetch.jsx
@@ -3,13 +3,24 @@ import React, { useState, useEffect } from "react";
 function MiniFetch() {
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // fetch here
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => setUsers(data));
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
   }, []);
+  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase()),
+  );
 
   return (
     <div>
@@ -20,13 +31,11 @@ function MiniFetch() {
         placeholder="Search users..."
       />
       <ul>
-        {users
-          .filter((user) =>
-            user.name.toLowerCase().includes(search.toLowerCase()),
-          )
-          .map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
+        {filteredUsers.length === 0 ? (
+          <li>No users found</li>
+        ) : (
+          filteredUsers.map((user) => <li key={user.id}>{user.name}</li>)
+        )}
       </ul>
     </div>
   );
