97c53b2250c02c7fe4edb95598015dba841a5968
abraaogg <abraaogoncalves1022@gmail.com>
2026-03-03 15:01:33 -0300

    css

--- react-todo-list (prototype)/src/App.jsx
+++ react-todo-list (prototype)/src/App.jsx
@@ -27,16 +27,30 @@ function App() {
     },
   ]);
 
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <div className="app">
         <h1>Lista de Tarefas</h1>
         <div className="todo-list">
           {todos.map((todo) => (
-           <Todo key={todo.id} todo = {todo}/>
+            <Todo key={todo.id} todo={todo} />
           ))}
         </div>
-        <TodoForm/>
+        <TodoForm addTodo={addTodo} />
       </div>
     </>
   );
