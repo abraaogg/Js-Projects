f1595d908ff0a54b150b7d99b8848e39ed627738
abraaogg <abraaogoncalves1022@gmail.com>
2026-03-17 11:05:08 -0300

    addButton

--- react-todo-list/src/components/Form.jsx
+++ react-todo-list/src/components/Form.jsx
@@ -12,17 +12,21 @@ function Form({ addTodo }) {
   return (
     <div>
       <h3>Add a new Todo</h3>
-      {open ? (
-        <div className="addButton">
-          <input value={text} onChange={(e) => setText(e.target.value)} />
+      <div className="createTodo">
+        {open ? (
+          <div className="addButton">
+            <input value={text} onChange={(e) => setText(e.target.value)} />
 
-          <button onClick={handleAddTodo}>Add</button>
-        </div>
-      ) : (
-        <button onClick={() => setOpen(true)}>+</button>
-        // We wrap       () => the function so it only runs when clicked
-      )}
-      {open && <button onClick={() => setOpen(false)}>Close</button>}
+            <button onClick={handleAddTodo}>Add</button>
+          </div>
+        ) : (
+          <button className="add" onClick={() => setOpen(true)}>
+            +
+          </button>
+          // We wrap       () => the function so it only runs when clicked
+        )}
+        {open && <button onClick={() => setOpen(false)}>Close</button>}
+      </div>
     </div>
   );
 }
