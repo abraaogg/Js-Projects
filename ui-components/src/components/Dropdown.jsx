71cf9c4590f175e40896aa0483c4da20a85661e6
abraaogg <abraaogoncalves1022@gmail.com>
2026-03-30 15:59:56 -0300

    dropdown

--- ui-components/src/components/Dropdown.jsx
+++ ui-components/src/components/Dropdown.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import "../css/dropdown.css"
+import React, { useState, useRef, useEffect } from "react";
+import "../css/dropdown.css";
 
 function Dropdown() {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState("Select");
+  const dropdownRef = useRef(null);
+  useEffect(() => {
+    function handleClick(event) {
+      if (!dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
   return (
-    <div>Dropdown</div>
-  )
+    <div ref={dropdownRef}>
+      <button onClick={() => setOpen(!open)}>{selected}</button>
+      {open && (
+        <div>
+          <p
+            onClick={() => {
+              setSelected("Option1");
+              setOpen(false);
+            }}
+          >
+            option 1
+          </p>
+          <p
+            onClick={() => {
+              setSelected("Option2");
+              setOpen(false);
+            }}
+          >
+            option 2
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Dropdown
+export default Dropdown;
