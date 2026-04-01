d6c0a1d919c80ad4e2bc832132b0ffad9e42bd51
abraaogg <abraaogoncalves1022@gmail.com>
2026-04-01 15:47:24 -0300

    Update Dropdown.jsx

--- ui-components/src/components/Dropdown.jsx
+++ ui-components/src/components/Dropdown.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useRef, useEffect } from "react";
 import "../css/dropdown.css";
 
-function Dropdown({options}) {
+function Dropdown({ options }) {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState("Select");
+  const [search, setSearch] = useState("");
   const dropdownRef = useRef(null);
   useEffect(() => {
     function handleClick(event) {
-      if (!dropdownRef.current.contains(event.target)) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
         setOpen(false);
       }
     }
@@ -17,22 +18,34 @@ function Dropdown({options}) {
       document.removeEventListener("click", handleClick);
     };
   }, []);
+
+  const filteredOptions = options.filter((option) => {
+    return option.toLowerCase().includes(search.toLowerCase());
+  });
   return (
     <div ref={dropdownRef}>
       <button onClick={() => setOpen(!open)}>{selected}</button>
       {open && (
         <div>
-          {options.map((option) => (
-            <p
-              onClick={() => {
-                setSelected(option);
-                setOpen(false);
-              }}
-              key={option}
-            >
-              {option}
-            </p>
-          ))}
+          <input
+            value={search}
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          {filteredOptions.length === 0
+            ? "No results found"
+            : filteredOptions.map((option) => (
+                <p
+                  onClick={() => {
+                    setSelected(option);
+                    setOpen(false);
+                    setSearch("");
+                  }}
+                  key={option}
+                >
+                  {option}
+                </p>
+              ))}
         </div>
       )}
     </div>
