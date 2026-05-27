e59c53e84207f8a0d64b96a32ac4a35bc9fd937b
abraaogg <abraaogoncalves1022@gmail.com>
2026-05-27 10:54:09 -0300

    portfolio-update

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,7 +1,7 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
