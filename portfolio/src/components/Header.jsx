b8598bd637d6cf874e80fab043a776e14a678fae
abraaogg <abraaogoncalves1022@gmail.com>
2026-06-02 11:19:23 -0300

    update

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function Header() {
+  return (
+    <div>
+      <a href="" className="logo">
+        Abraão <span>Grigório</span>
+      </a>
+    </div>
+  );
+}
+
+export default Header;
