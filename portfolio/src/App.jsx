bef2ebb1c99bf9f75b57f43fd46cd3437c41fd14
abraaogg <abraaogoncalves1022@gmail.com>
2026-06-16 15:16:36 -0300

    portfolio

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return <>
   <Header/>
+  <Home/>
+  <Projects/>
   </>;
 }
 
---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className='projects'>
+        <h2>Hii</h2>
+    </div>
+  )
+}
+
+export default Projects
