25f74dcc8f25d66ad56541d736dbee501b466dd6
abraaogg <abraaogoncalves1022@gmail.com>
2026-06-23 16:18:14 -0300

    jsx

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../styles/home.css";
 import { FaGithub, FaFileAlt } from "react-icons/fa";
 
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import "../styles/projects.css";
 
 function Projects() {
+  const projects = [
+    {
+      id: 1,
+      image: "/portfolio.png",
+      title: "Portfolio",
+      description: "Personal portfolio built with React.",
+      github: "https://github.com/...",
+    },
+    {
+      id: 2,
+      image: "/portfolio.png",
+      title: "Portfolio",
+      description: "Personal portfolio built with React.",
+      github: "https://github.com/...",
+    },
+    {
+      id: 3,
+      image: "/portfolio.png",
+      title: "Portfolio",
+      description: "Personal portfolio built with React.",
+      github: "https://github.com/...",
+    },
+  ];
   return (
-    <div className='projects'>
-        <h2>Hii</h2>
+    <div className="projects">
+      <h2>PROJECTS</h2>
+      <div className="cards">
+        {projects.map((project) => (
+          <div className="card">
+            <h3>{project.title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
