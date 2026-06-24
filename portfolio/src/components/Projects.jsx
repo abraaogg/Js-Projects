bc277f3d1845ee508482f45aacb64d09d21106a9
abraaogg <abraaogoncalves1022@gmail.com>
2026-06-24 10:37:42 -0300

    projects.jsx

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,24 +1,27 @@
 import "../styles/projects.css";
+import notesImg from "../imgs/notes.webp";
+import todoImg from "../imgs/todo.webp";
+import weatherImg from "../imgs/weather.webp";
 
 function Projects() {
   const projects = [
     {
       id: 1,
-      image: "/portfolio.png",
+      image: notesImg,
       title: "Portfolio",
       description: "Personal portfolio built with React.",
       github: "https://github.com/...",
     },
     {
       id: 2,
-      image: "/portfolio.png",
+      image: todoImg,
       title: "Portfolio",
       description: "Personal portfolio built with React.",
       github: "https://github.com/...",
     },
     {
       id: 3,
-      image: "/portfolio.png",
+      image: weatherImg,
       title: "Portfolio",
       description: "Personal portfolio built with React.",
       github: "https://github.com/...",
@@ -29,8 +32,13 @@ function Projects() {
       <h2>PROJECTS</h2>
       <div className="cards">
         {projects.map((project) => (
-          <div className="card">
+          <div className="card" key={project.id}>
+            <img src={project.image} alt={project.title} />
             <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <a href={project.github} target="_blank">
+              GitHub
+            </a>
           </div>
         ))}
       </div>
