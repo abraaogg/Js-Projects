3f4a51f5353750f546fa7a166e516df2935b0114
abraaogg <abraaogoncalves1022@gmail.com>
2026-08-24 10:15:03 -0300

    minimal-watch

---
+++ minimal-watch/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+
+function Header() {
+  return (
+    <header className="header" id="header">
+      <div className="logo">
+        MyLogo
+      </div>
+
+      <nav className="nav">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
