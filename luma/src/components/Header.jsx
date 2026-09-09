42115129e1480936bb4de0af72ad1c860ee9d256
abraaogg <abraaogoncalves1022@gmail.com>
2026-09-09 17:33:08 -0300

    luma

--- luma/src/components/Header.jsx
+++ luma/src/components/Header.jsx
@@ -1,9 +1,18 @@
-import "../styles/header.css"
+import "../styles/header.css";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <a href="">LOGO</a>
+      <nav className="nav">
+        <a href="">Services</a>
+        <a href="">Team</a>
+        <a href="">Reviews</a>
+        <a href="">Visit</a>
+      </nav>
+      <a href="">Book a visit</a>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- luma/src/components/Home.jsx
+++ luma/src/components/Home.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import homeBackground from "../images/home-background.png";
 import "../styles/home.css";
 
 function Home() {
   return (
     <div className="home">
-      <img
-        src={homeBackground}
-        alt="Home background"
-        className="home-background"
-      />
     </div>
   );
 }
