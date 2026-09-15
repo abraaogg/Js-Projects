9f8fd1b3472c6f6f9971f9b22186fe447eaf4cad
abraaogg <abraaogoncalves1022@gmail.com>
2026-09-15 11:19:48 -0300

    header

---
+++ carlos-ebook/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/header.css"
+function Header() {
+  return (
+    <div className="header">
+      <h1>Carlos Foa</h1>
+      <a href="">Comprar e-book</a>
+    </div>
+  );
+}
+
+export default Header;
